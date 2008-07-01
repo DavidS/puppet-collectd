61c065f62af6064a06fbedbdfdeee92cd6aac349
David Schmitt <david@dasz.at>
2008-07-01 10:04:04 +0000

    Add <Plugin> support for collectd_conf
    This implements a basic hand-through for <Plugin> sections in the
    collectd.conf. With this change Debian's default collectd.conf can
    be parsed completely.

--- plugins/puppet/provider/collectd_conf/parsed.rb
+++ plugins/puppet/provider/collectd_conf/parsed.rb
@@ -9,6 +9,21 @@ Puppet::Type.type(:collectd_conf).provide(:parsed,
 
 	text_line :blank, :match => %r{^\s*$}
 
+	plugin = record_line :plugin_start, :fields => %w{name},
+		:match => %r{^\s*<Plugin\s+(.*)\s*>\s*$}
+
+	class << plugin
+		def to_line(record)
+			"<Plugin %s>\n%s\n</Plugin>" % [
+				record[:name],
+				record[:contents].join("\n")
+			]
+		end
+	end
+
+	record_line :plugin_end, :fields => [],
+		:match => %r{^\s*</Plugin>\s*$}
+
 	config = record_line :parsed, :fields => %w{name value},
 		:match => %r{^\s*(\S+)\s+(.*)$}
 
@@ -26,33 +41,53 @@ Puppet::Type.type(:collectd_conf).provide(:parsed,
 	def self.prefetch_hook(records)
 
 		sorted_records = {}
+		plugins = []
 
+		plugin = nil
+		contents = nil
 		records.each { |record|
 
-			value = nil
-			if record[:value]
-				unless record[:value].is_a?(Array) 
-					value = [ record[:value] ]
+			case record[:record_type]
+			when :plugin_start
+				# start collecting lines within the <Plugin>
+				plugin = record
+				contents = []
+			when :plugin_end
+				# save the collected records
+				plugin[:contents] = contents
+				plugins << plugin
+				plugin = nil
+				contents = nil
+			when :parsed
+				if contents
+					# collect the record
+					contents << to_line(record)
 				else
-					value = record[:value]
+					# process this record
+					value = nil
+					if record[:value]
+						unless record[:value].is_a?(Array) 
+							value = [ record[:value] ]
+						else
+							value = record[:value]
+						end
+					end
+			
+					real_record = nil
+					if sorted_records[record[:name]]
+						real_record = sorted_records[record[:name]]
+						value += real_record[:value]
+					else
+						real_record = record
+					end
+			
+					real_record[:value] = value
+					sorted_records[record[:name]] = real_record
 				end
 			end
-
-			real_record = nil
-			if sorted_records[record[:name]]
-				real_record = sorted_records[record[:name]]
-				value += real_record[:value]
-			else
-				real_record = record
-			end
-
-			real_record[:value] = value
-			sorted_records[record[:name]] = real_record
 		}
 
-		sorted_records.values.collect { |record|
-			record
-		}
+		sorted_records.values + plugins
 	end
 end
 		
