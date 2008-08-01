397750d344e1bb636e7e4e11138707174ce821c8
David Schmitt <david@dasz.at>
2008-08-01 11:15:13 +0000

    un-conflict multiple logfiles

--- manifests/logfile.pp
+++ manifests/logfile.pp
@@ -1,7 +1,7 @@
 
 define collectd::logfile($level, $timestamp = 'true') {
 	collectd::plugin {
-		'logfile':
+		"logfile_${name}":
 			lines => [
 				"File \"/var/log/collectd_${name}.log\"",
 				"Timestamp ${timestamp}",
